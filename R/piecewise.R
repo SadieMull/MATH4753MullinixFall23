0fd75b61ebd64261b9fe76b3fcce3f015f8832a5
SadieMull <sadie.mullinix@gmail.com>
2023-09-15 12:31:03 -0500

    Updated Piecewise skeleton

--- R/piecewise.R
+++ R/piecewise.R
@@ -1,13 +1,14 @@
 #' Piecewise function at x = 18
 #'
 #' @param x the variable in the equation
-#' @param coef a coefficent from the data summary
+#' @param coef a coefficient from the lm summary
 #'
 #' @return a one line piecewise function where the break is at x = 18
 #' @export
 #'
 #' @examples
-#' \dontrun{myf(x,coef=tmp$coefficients[,"Estimate"])}
+#' \dontrun{piecewise(x,coef=tmp$coefficients[,"Estimate"])}
 piecewise = function(x,coef){
   coef[1]+coef[2]*(x) + coef[3]*(x-18)*(x-18>0)
+
 }
